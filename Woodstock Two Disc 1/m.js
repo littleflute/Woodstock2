a3c518ecec2abb6b5acc38989a9baa874618041a
littleflute <13699175164@sina.cn>
2017-05-30 15:37:26 -0600

    Add files via upload

---
+++ Woodstock Two Disc 1/m.js
@@ -0,0 +1,8 @@
+var f = [];				
+f[0]	= "v0.0.7";			
+f[1]	= "01 Jam Back at the House.mp3";		
+f[2]	= "02 Izabella.mp3";		
+f[3]	= "03 Get My Heart Back Together.mp3";		
+f[4]	= "04 Saturday Afternoon-Won't You Try.mp3";		
+f[5]	= "05 Eskimo Blue Day.mp3";		
+f[6]	= "06 Everything's Gonna Be Alright.mp3";		
